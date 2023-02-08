f29cea18aa49832ad6e362f26b17c62b96a19b08
holywater93 <ssk7870@naver.com>
2023-02-08 18:28:44 +0900

    성수's 로그인폼 202302081828

---
+++ SB_today_table/src/main/java/com/ezen/todaytable/dto/MemberVO.java
@@ -0,0 +1,23 @@
+package com.ezen.todaytable.dto;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	
+	private String id; // 아이디
+	private String pwd; // 비밀번호
+	private String name; // 이름
+	private String phone; // 전화번호
+	private String email; // 이메일
+	private String nick; // 닉네임
+	private String address1; // 주소1
+	private String address2; // 주소2
+	private String zip_num; // 집우편주소?
+	private Timestamp indate; // 가입날짜?
+	private String img; // 프로필사진?
+	private String useyn; // 휴면회원 여부
+
+}
