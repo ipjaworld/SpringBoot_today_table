16a84564399888e404f7a61ca21dd52b7413e26b
Minjae Lee <ipjaworld@gmail.com>
2023-02-09 18:50:18 +0900

    3 way merge test 2

--- SB_today_table/procedure_gh.sql
+++ SB_today_table/procedure_gh.sql
@@ -1,13 +1,3 @@
--- 겟 올 레시피
-create or replace procedure getAllRecipe(
-    p_cur OUT SYS_REFCURSOR
-)
-is 
-begin
-    open p_cur FOR
-        select * from recipe_page_view;
-end;
-
 -- 레시피 배너 테이블 생성
 create table recipebanner(
     bseq number(5) primary key,
@@ -17,6 +7,16 @@ create table recipebanner(
     useyn char(1) default 'y',
     indate date default sysdate
 );
+
+-- 겟 배너 레시피
+CREATE OR REPLACE PROCEDURE getRecipeBannerList(
+    p_cur OUT   SYS_REFCURSOR
+)
+IS
+BEGIN
+    open p_cur for
+        select * from recipebanner order by useyn desc, order_seq asc;
+END;
 
 -- 레시피 배너 시퀀스 생성
 create sequence tt_banner_seq start with 1;
@@ -52,6 +52,14 @@ BEGIN
         select * from recipebanner order by useyn desc, order_seq asc;
 END;
 
-
+-- 레시피 배너 테이블 생성
+create table recipebanner(
+    bseq number(5) primary key,
+    name varchar2(100),
+    order_seq number(1),
+    image varchar2(50),
+    useyn char(1) default 'y',
+    indate date default sysdate
+);
 
 
