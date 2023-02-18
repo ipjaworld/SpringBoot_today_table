127dee10c8d0d3599024cb8e5128c05ca4d71397
Geonha Lee <ipjaworld@gmail.com>
2023-02-18 12:04:02 +0900

    건하 scrollAnimation 추가

--- SB_today_table/src/main/resources/static/script/main-gh.js
+++ SB_today_table/src/main/resources/static/script/main-gh.js
@@ -76,6 +76,50 @@ function footer_go_to( destination ){
 
 
 
+/**
+ * scrollAnimation 설명서
+ * 이 메서드는 화면(window)의 스크롤에 맞춰 특정 요소에
+ * 이벤트(show, hide, transform) 를 줄 수 있습니다.
+ * 제이쿼리 임포트를 필수로 요구합니다.
+ * show_and_hide 클래스를 이벤트를 적용할 오브젝트에 적용하면 됩니다.
+ * _checkPosition에 add, remove에 있는 클래스는 자기 상황에 맞게 바꾸면 됩니다.
+ */
+const scrollAnimation=()=>{
+    // items는 나중에 querySelectorAll을 사용할 때 쓸 수 있습니다
+    // actionHeight는 가로 길이로 얼마나 스크롤했을 때 이벤트가 발동할지
+    let items, actionHeight;
+
+    // 페이지가 로드될 때 아이템에 쿼리셀렉터를 실행하고
+    // actionHeight
+    const initModule=()=>{
+        items = document.querySelectorAll(".show_and_hide");
+        actionHeight = 600;
+        _addEventHandlers();
+    };
+
+    const _addEventHandlers=()=>{
+        window.addEventListener("scroll", _checkPosition);
+        window.addEventListener("load", _checkPosition);
+        window.addEventListener("resize", initModule);
+    };
+
+    const _checkPosition=()=>{
+        let here = window.scrollY;
+        // if 조건일 때 사라지게 하고
+        if(here>actionHeight){
+            $('.show_and_hide').removeClass('show').addClass('hide');  
+        }else{
+            // else 조건일 때 보여지게 하는 코드입니다.
+            $('.show_and_hide').removeClass('hide').addClass('show');
+        }
+    }
+    return {
+        init: initModule
+    }
+}
+
+// 함수 초기화를 시켜줍니다.
+scrollAnimation().init();
 
 
 
